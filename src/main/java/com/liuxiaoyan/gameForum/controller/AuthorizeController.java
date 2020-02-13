74eed2900e6e6a4d70155ba4413010b9a6aed269
superman <944149781@qq.com>
2020-02-13 13:17:16 +0800

    2 13

--- src/main/java/com/liuxiaoyan/gameForum/controller/AuthorizeController.java
+++ src/main/java/com/liuxiaoyan/gameForum/controller/AuthorizeController.java
@@ -1,14 +1,31 @@
 package com.liuxiaoyan.gameForum.controller;
 
+import com.liuxiaoyan.gameForum.dto.AccessTokenDTO;
+import com.liuxiaoyan.gameForum.dto.GithubUser;
+import com.liuxiaoyan.gameForum.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("511f20dc01d20cf7452f");
+        accessTokenDTO.setClient_secret("4de6550418d44d8340d2cf92cbe7e4c4e933ee8b");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
